c1145825c65b87a47ed71c3cf7f7e6f0175e2d12
L Lumbreras <lumbrerl@gmail.com>
2013-03-27 15:31:08 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InSightQuery::Application.config.secret_token = '91e1aef4f7d7044edc985cb33440ef6b439fa3d167c15b44028752f79cd4cf801435645303c49c2b577c5b42f6fa4885af3e348c7659f9399e67f35a78113f84'
